f067b9bf5e9d3fc8e4fcb99adfac5150b7bd4256
farhangemad <56489056+farhangemad@users.noreply.github.com>
2019-10-20 20:20:48 -0400

    Add files via upload

---
+++ src/out.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+//Printing results
+
+int first(double x1) {
+	char str[50];
+	sprintf(str, "The first root = %lf\n", dbl);
+	puts(str)
+
+	return(0)
+}
+
+int second(double x2) {
+	char str[50];
+	sprintf(str, "The second root = %lf\n", dbl);
+	puts(str)
+
+	return(0)
+}
