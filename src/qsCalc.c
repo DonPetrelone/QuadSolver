94d36ef13347897348f8ce552324e6fba992b762
dylanlafleur-wmu <dtf5785@wmich.edu>
2019-10-28 10:41:35 -0400

    Update qsCalc.c

--- src/qsCalc.c
+++ src/qsCalc.c
@@ -1,25 +1,25 @@
 #include "qsCalc.h"
 
 int calcOutput(qsVars *vars) {
-  int r = 0;
+  int ret = 0;
   int d;
 
   if (calcDiscriminant(vars, &d) || vars-> a == 0)        // adjust r for different outcomes?
-    r = -1;
+    ret = -1;
   else {
     vars->x1 = (-1 * vars->b + sqrt((double)d)) / (2 * vars->a);
     vars->x2 = (-1 * vars->b + sqrt((double)d)) / (2 * vars->a);
   }
 
-  return r;
+  return ret;
 }
 
 int calcDiscriminant(qsVars *vars, int *d) {
-  int r = 0;
+  int ret = 0;
   *d = vars->b * vars->b - 4 * vars->a * vars->c;
 
   if (*d < 0)
-    r = -1;
+    ret = -1;
 
-  return r;
+  return ret;
 }
