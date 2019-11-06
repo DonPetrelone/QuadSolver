b5ac18c3a5463f29184d594a253f60417e5461d8
DonPetrelone <vinicius.petrellicicerone@wmich.edu>
2019-11-06 17:41:00 -0500

    all spikes

--- spikes/spike2.c
+++ spikes/spike2.c
@@ -1,7 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 int main ()
 {
+    printf("%s\n", "Type the first floating point");
 
+    float a = 0.0;
+    scanf ("%f", &a);
+
+    if (a == INFINITY)
+    {
+        printf("%s\n", "It is infinity");
+    }
+    else
+    {
+        printf("%s\n", "It is not infinity");
+    }
 }
--- spikes/spike3.c
+++ spikes/spike3.c
@@ -1,7 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 int main ()
 {
+    printf("%s\n", "Type the first floating point");
+
+    float a = 0.0;
+    scanf ("%f", &a);
+
+    if (a != a)
+    {
+        printf("%s\n", "nan");
+    }
+    else
+    {
+        printf("%s\n", "NOT nan");
+    }
 
 }
