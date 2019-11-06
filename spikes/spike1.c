72e0c15eac6a058abfaf703f80ffa102a89f57c9
DonPetrelone <vinicius.petrellicicerone@wmich.edu>
2019-11-06 17:33:01 -0500

    commit spike1 and makefile

--- spikes/spike1.c
+++ spikes/spike1.c
@@ -1,31 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 int main ()
 {
     printf("%s\n", "Type the first floating point");
 
-    double a;
-    scanf ("%f", a);
+    float a = 0.0;
+    scanf ("%f", &a);
 
     printf("%s\n", "Type the second floating point");
 
-    double b;
-    scanf ("%f", b);
+    float b = 0.0;
+    scanf ("%f", &b);
 
     printf("%s\n", "Type the level of precision");
 
-    double p;
-    scanf ("%f", p);
+    float p = 0.0;
+    scanf ("%f", &p);
 
-    if (a - b > p)
-    {
-        //considered not equal
-        printf("%s\n", "They are not equal.");
-    }
-    else if (a - b < P)
+    printf("%f\n", fabs(a - b));
+
+    if (fabs(a - b) <= p)
     {
         //considered equal
         printf("%s\n", "They are equal.");
     }
+    else
+    {
+        //not equal
+        printf("%s\n", "not equal");
+    }
+
 }
